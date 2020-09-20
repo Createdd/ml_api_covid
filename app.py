a2a54b28e48010a91ff17d8a9c66bf81da6a74f9
Daniel Deutsch <deudan1010@gmail.com>
2020-09-20 12:41:22 +0200

    Add python version of notebook

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, request, url_for, redirect, render_template, jsonify
-from joblib import dump, load
-# import pandas as pd
-# import pickle
-# import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+from joblib import load
+from sklearn.preprocessing import LabelEncoder
+
+available_countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bonaire Sint Eustatius and Saba', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo', 'Costa Rica', "Cote d'Ivoire", 'Croatia', 'Cuba', 'Curacao', 'Cyprus', 'Czech Republic', 'Democratic Republic of Congo', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Faeroe Islands', 'Falkland Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'International', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Somalia', 'South Africa', 'South Korea', 'South Sudan', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Timor', 'Togo', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turks and Caicos Islands', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Virgin Islands', 'Uruguay', 'Uzbekistan', 'Vatican', 'Venezuela', 'Vietnam', 'Western Sahara', 'World', 'Yemen', 'Zambia', 'Zimbabwe']
+
+
+encoder = LabelEncoder()
+rf = load('rf_model.joblib')
 
 app = Flask(__name__)
+
+def pre_process(df):
+    nominal = df.select_dtypes(include=['object']).copy()
+    nominal_cols = nominal.columns.tolist()
+
+    for col in nominal_cols:
+        col
+        if df[col].isna().sum() > 0:
+            df[col].fillna('MISSING', inplace=True)
+        df[col] = encoder.fit_transform(df[col])
+
+    numerical = df.select_dtypes(include=['float64']).copy()
+
+    for col in numerical:
+        df[col].fillna((df[col].mean()), inplace=True)
+
+    X = df.drop(columns=['new_cases'])
+    y = df.new_cases
+
+    return X, y
 
 @app.route('/')
 def home():
@@ -12,18 +37,24 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    int_features = [x for x in request.form.values()]
-    final = np.array(int_features)
-    data_unseen = pd.DataFrame([final], columns = cols)
-    prediction = predict_model(model, data=data_unseen, round = 0)
+    input_val = [x for x in request.form.values()][0]
+    url_to_covid = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
+    encoder = LabelEncoder()
 
+    if input_val not in available_countries:
+        return 'Country is not in available list. Try one from the list! Go back in your browser', 500
 
-    prediction = int(prediction.Label[0])
-    prediction = clf.predict(query)
+    df_orig = pd.read_csv(url_to_covid)
+    _ = encoder.fit_transform(df_orig['location'])
+    encode_ind = (encoder.classes_).tolist().index(input_val)
+    df_orig[df_orig.location == input_val]
 
-    return jsonify({'prediction': list(prediction)})
-    # return render_template('home.html',pred='New cases will be{}'.format(prediction))
+    X, _ = pre_process(df_orig)
+    to_pred = X[X.location == encode_ind].iloc[-1].values.reshape(1,-1)
+
+    prediction = rf.predict(to_pred)[0]
+
+    return render_template('home.html',pred=f'New cases will be {prediction}')
 
 if __name__ == '__main__':
-    rf = joblib.load('rf_model.joblib')
     app.run(debug=True)
