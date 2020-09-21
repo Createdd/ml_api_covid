a116b5b218c39655dcad6c14ad5ce894bb90c397
Daniel Deutsch <deudan1010@gmail.com>
2020-09-21 12:03:55 +0200

    Add api functionality

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import pandas as pd
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from joblib import load
 from sklearn.preprocessing import LabelEncoder
 
 available_countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bonaire Sint Eustatius and Saba', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo', 'Costa Rica', "Cote d'Ivoire", 'Croatia', 'Cuba', 'Curacao', 'Cyprus', 'Czech Republic', 'Democratic Republic of Congo', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Faeroe Islands', 'Falkland Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'International', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania', 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten (Dutch part)', 'Slovakia', 'Slovenia', 'Somalia', 'South Africa', 'South Korea', 'South Sudan', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Timor', 'Togo', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turks and Caicos Islands', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Virgin Islands', 'Uruguay', 'Uzbekistan', 'Vatican', 'Venezuela', 'Vietnam', 'Western Sahara', 'World', 'Yemen', 'Zambia', 'Zimbabwe']
-
-
+url_to_covid = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
 encoder = LabelEncoder()
 rf = load('rf_model.joblib')
 app = Flask(__name__)
@@ -44,8 +43,6 @@ def pre_process(df):
 
 
 def get_prediction_params(input_val, url_to_covid):
-    encoder = LabelEncoder()
-
     df_orig = pd.read_csv(url_to_covid)
     _ = encoder.fit_transform(df_orig['location'])
     encode_ind = (encoder.classes_).tolist().index(input_val)
@@ -64,16 +61,28 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     input_val = [x for x in request.form.values()][0]
-    url_to_covid = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
-
 
     if input_val not in available_countries:
-        return 'Country is not in available list. Try one from the list! Go back in your browser', 500
+        return f'Country {input_val} is not in available list. Try one from the list! Go back in your browser', 400
 
     to_pred = get_prediction_params(input_val, url_to_covid)
     prediction = rf.predict(to_pred)[0]
 
     return render_template('home.html',pred=f'New cases will be {prediction}')
+
+
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    input_val = request.form.get('location')
+    if input_val not in available_countries:
+        return f'Country {input_val} is not in available list. Try one from the list! Go back in your browser', 400
+
+    to_pred = get_prediction_params(input_val, url_to_covid)
+    prediction = rf.predict(to_pred)[0]
+
+
+    return jsonify(prediction)
 
 
 if __name__ == '__main__':
